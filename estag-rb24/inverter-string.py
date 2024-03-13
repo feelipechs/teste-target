0ef9c2f77c13a57174f8e68cb6feab79b676dc1d
chagas <155638523+feelipechs@users.noreply.github.com>
2024-03-13 10:28:09 -0300

    Update inverter-string.py

--- estag-rb24/inverter-string.py
+++ estag-rb24/inverter-string.py
@@ -1,2 +1,3 @@
 text = input("Vamos inverter as strings. Digite um texto, nome ou qualquer coisa do tipo: ").strip().lower()
 print(text[::-1])
+# Fiquei em dúvida se era pra inverter palavra por palavra, ou a frase inteira.
